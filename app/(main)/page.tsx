d5f5218bc86716a37b7cf653ad2691a08b88fb7d
Runanka <runanka@outlook.com>
2024-05-25 17:26:34 +0530

    [feat] add animated background wave

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,15 +1,13 @@
-import Coffeewave from "./_components/coffeewave";
+
 import LoadingPage from "./_loading/loadingPage";
 import HomePage from "./homePage/homePage";
 import MainCard from "./maincard/mainCard";
 
 export default function MyApp() {
   return (
-    <div className="z-0 w-screen h-screen">
-      <LoadingPage />
-      <Coffeewave />
-      {/* <HomePage />
-      <MainCard /> */}
+    <div className="z-0 h-screen w-screen">
+      {/* <LoadingPage /> */}
+      <HomePage />
     </div>
   );
 }
